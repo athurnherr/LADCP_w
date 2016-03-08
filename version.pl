f3c163ad8f51128bc19b510c3e4bbcdf8b03714f
A.M. Thurnherr <athurnherr@yahoo.com>
2016-03-08 16:21:50 +0000

    V1.2beta

--- version.pl
+++ version.pl
@@ -1,19 +1,20 @@
 #======================================================================
 #                    V E R S I O N . P L 
 #                    doc: Tue Oct 13 10:40:57 2015
-#                    dlm: Mon Jan 25 09:28:36 2016
+#                    dlm: Tue Mar  8 15:57:19 2016
 #                    (c) 2015 A.M. Thurnherr
-#                    uE-Info: 12 0 NIL 0 0 72 0 2 4 NIL ofnI
+#                    uE-Info: 12 52 NIL 0 0 72 0 2 4 NIL ofnI
 #======================================================================
 
 # HISTORY:
 #	Oct 13, 2015: - created
 #	Jan  4, 2016: - added $ADCP_tools_minVersion
+#	Mar  8, 2016: - updated antsMinLibVersion to 6.3
 
 #$VERSION = '1.1beta1';			# Oct 12, 2015
 #$VERSION = '1.1';				# Jan  4, 2016
 $VERSION = '1.2beta';			# Jan  5, 2016
 
-$antsMinLibVersion 		= 6.2;
+$antsMinLibVersion 		= 6.3;
 $ADCP_tools_minVersion 	= 1.4;
 
