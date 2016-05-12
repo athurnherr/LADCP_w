b65966346a058b3f0aaffbe7cd6de9d7974513b3
A.M. Thurnherr <athurnherr@yahoo.com>
2016-05-12 10:54:28 -0400

    V1.2

--- version.pl
+++ version.pl
@@ -1,9 +1,9 @@
 #======================================================================
 #                    V E R S I O N . P L 
 #                    doc: Tue Oct 13 10:40:57 2015
-#                    dlm: Wed Mar 30 09:27:25 2016
+#                    dlm: Thu May 12 10:46:27 2016
 #                    (c) 2015 A.M. Thurnherr
-#                    uE-Info: 16 29 NIL 0 0 72 0 2 4 NIL ofnI
+#                    uE-Info: 21 46 NIL 0 0 72 0 2 4 NIL ofnI
 #======================================================================
 
 # HISTORY:
@@ -14,9 +14,11 @@
 #	Mar 29, 2016: - updated antsMinLibVersion to 6.6 (libSBE bugs)
 #				  - update tools to 1.5 (obsolete getopts)
 #	Mar 30, 2016: - V1.2beta7
+#	Apr 16, 2016: - V1.2beta8
+#	May 12, 2016: - V1.2
 
 #$VERSION = '1.1';				# Jan  4, 2016
-$VERSION = '1.2beta7';
+$VERSION = '1.2';				# May 12, 2016
 
 $antsMinLibVersion 		= 6.6;
 $ADCP_tools_minVersion 	= 1.5;
